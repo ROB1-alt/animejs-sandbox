cf3bbc1b6273a493faaa372e5518defb0623b345
Robin <59090826+ROB1-alt@users.noreply.github.com>
2021-04-19 10:14:09 +0200

    initial commit

---
+++ script.js
@@ -0,0 +1,16 @@
+var domAttributes = anime({
+  targets: '#domAttributes input',
+  delay: 30,
+  value: 100,
+  round: 1,
+  easing: 'easeInOutExpo'
+
+});
+
+var test = anime({
+  targets : '#test',
+  translateX : 230,
+  delay: 300,
+})
+
+
